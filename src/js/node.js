755d2d6f731a49c41e14d51880a48de96b4544de
SH_JT <1225754321@qq.com>
2024-05-27 07:05:38 +0800

    test找到一个可用的算法

--- src/js/node.js
+++ src/js/node.js
@@ -1,41 +1,76 @@
-class Dot {
-    constructor(id, data, size, x, y) {
-        this.id = id
-        this.data = data
-        this.size = size
-        this.x = x
-        this.y = y
-        this.is_fixed = false
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    plus(other) {
+        return new Vec(this.x + other.x, this.y + other.y);
+    }
+    minus(other) {
+        return new Vec(this.x - other.x, this.y - other.y);
+    }
+    times(factor) {
+        return new Vec(this.x * factor, this.y * factor);
+    }
+    get length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
     }
 }
 
-class Line {
-    constructor(source, target) {
-        this.source = source
-        this.target = target
+class GraphNode {
+    constructor() {
+        this.pos = new Vec(Math.random() * 1000, Math.random() * 1000);
+        this.edges = [];
     }
+
+    connect(other) {
+        this.edges.push(other);
+        other.edges.push(this);
+    }
+
+    hasEdge(other) {
+        return this.hasEdgeFast(other)
+    }
+
 }
 
-function start(dots, lines, height = 1920, width = 1080) {
-    let m_dots = {};
-    let sources = {};
-    let targets = {};
-    dots.forEach(dot => {
-        m_dots[dot.id] = dot
-    });
-    lines.forEach(line => {
-        if (!sources[line.source]) {
-            sources[line.source] = []
+GraphNode.prototype.hasEdgeFast = function (other) {
+    for (let i = 0; i < this.edges.length; i++) {
+        if (this.edges[i] === other) return true
+    }
+    return false;
+}
+const springLength = 40; //弹簧静止长度
+const springStrength = 0.1; //弹性系数
+const repulsionStrength = 1500; //库伦常数
+const skipDistance = 175; // 距离过远跳过计算
+
+function forceDirected_noRepeat_skip_noVector(graph) {
+    for (let i = 0; i < graph.length; i++) {
+        let node = graph[i];
+        for (let j = i + 1; j < graph.length; j++) {
+            let other = graph[j];
+
+            let apartX = other.pos.x - node.pos.x;
+            let apartY = other.pos.y - node.pos.y;
+            let distance = Math.max(1, Math.sqrt(apartX * apartX + apartY * apartY))
+
+            let hasEdge = node.hasEdge(other);
+            if (!hasEdge && distance > skipDistance) continue;
+            let forceSize = -repulsionStrength / (distance * distance);
+            if (node.hasEdge(other)) {
+                forceSize += (distance - springLength) * springStrength;
+            }
+
+            let forceX = apartX * forceSize / distance;
+            let forceY = apartY * forceSize / distance;
+
+            node.pos.x += forceX;
+            node.pos.y += forceY;
+
+            other.pos.x -= forceX;
+            other.pos.y -= forceY;
+
         }
-        if (!targets[line.target]) {
-            targets[line.target] = []
-        }
-        sources[line.source].push(m_dots[line.target])
-        targets[line.target].push(m_dots[line.source])
-        m_dots[line.source].size += 1
-        m_dots[line.target].size += 1
-    });
-    dots.sort((a, b) => a.size - b.size)
-    // 最小圆半径
-    let br = 4; // 每多一个连接半径+0.1
+    }
 }
